9832b7584a8abeb58c3f78dd568a457fe91392ce
Matt Precious <mattp@squareup.com>
2020-07-12 14:51:38 -0400

    protogram 0.2.1-test

--- Formula/protogram.rb
+++ Formula/protogram.rb
@@ -1,8 +1,9 @@
 class Protogram < Formula
   desc "Quick and easy command line proto deserialization"
   homepage "https://github.com/mattprecious/protogram"
-  url "https://github.com/mattprecious/protogram/releases/download/0.2.0/protogram"
-  sha256 "f2e35f77bde11a2b5f5c867a95492d5733a2ecbba5caa6aac28c65ad1f10289e"
+  url "https://github.com/mattprecious/protogram/releases/download/0.2.1-test/protogram"
+  version "0.2.1-test"
+  sha256 "04b1ea33fb0a102ffa087465db61026f27c6ea952bf790baa817e03e34bbef80"
   head "https://github.com/mattprecious/protogram.git"
 
   bottle :unneeded
