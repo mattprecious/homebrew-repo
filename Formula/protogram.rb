b016da00a62d425475da0c60474dd4f92730f8bd
Matthew Precious <mattp@squareup.com>
2022-04-26 10:13:10 -0400

    Fix release url

--- Formula/protogram.rb
+++ Formula/protogram.rb
@@ -1,7 +1,7 @@
 class Protogram < Formula
   desc "Quick and easy command line proto deserialization"
   homepage "https://github.com/mattprecious/protogram"
-  url "https://github.com/mattprecious/protogram/releases/download/0.2.0/protogram"
+  url "https://github.com/mattprecious/protogram/releases/download/v0.2.0/protogram"
   sha256 "f2e35f77bde11a2b5f5c867a95492d5733a2ecbba5caa6aac28c65ad1f10289e"
   head "https://github.com/mattprecious/protogram.git"
 
